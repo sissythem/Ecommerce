8ea85b15640d0c47ab09c0d893e845049b6f24b0
vasilikivmo <vslkmoschou@gmail.com>
2017-04-29 16:00:40 +0300

    Netbeans Project

---
+++ Ecommerce/build/generated-sources/ap-source-output/ecommerce_rest/Messages_.java
@@ -0,0 +1,17 @@
+package ecommerce_rest;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-28T19:31:39")
+@StaticMetamodel(Messages.class)
+public class Messages_ { 
+
+    public static volatile SingularAttribute<Messages, Integer> conversationId;
+    public static volatile SingularAttribute<Messages, Integer> id;
+    public static volatile SingularAttribute<Messages, String> body;
+    public static volatile SingularAttribute<Messages, Date> timestamp;
+
+}
---
+++ Ecommerce/src/java/ecommerce_rest/Messages.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecommerce_rest;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author vasso
+ */
+@Entity
+@Table(name = "messages")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Messages.findAll", query = "SELECT m FROM Messages m"),
+    @NamedQuery(name = "Messages.findById", query = "SELECT m FROM Messages m WHERE m.id = :id"),
+    @NamedQuery(name = "Messages.findByConversationId", query = "SELECT m FROM Messages m WHERE m.conversationId = :conversationId"),
+    @NamedQuery(name = "Messages.findByBody", query = "SELECT m FROM Messages m WHERE m.body = :body"),
+    @NamedQuery(name = "Messages.findByTimestamp", query = "SELECT m FROM Messages m WHERE m.timestamp = :timestamp")})
+public class Messages implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "conversation_id")
+    private int conversationId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "body")
+    private String body;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+    public Messages() {
+    }
+
+    public Messages(Integer id) {
+        this.id = id;
+    }
+
+    public Messages(Integer id, int conversationId, String body, Date timestamp) {
+        this.id = id;
+        this.conversationId = conversationId;
+        this.body = body;
+        this.timestamp = timestamp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(int conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Messages)) {
+            return false;
+        }
+        Messages other = (Messages) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ecommerce_rest.Messages[ id=" + id + " ]";
+    }
+    
+}
