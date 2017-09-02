e1bffc710796444b467ddb37800551ee4a672b5d
cthem <c.themeli@gmail.com>
2017-09-02 19:13:27 +0300

    Slider

--- AirbnbProject/app/src/main/java/util/MyTextSlider.java
+++ AirbnbProject/app/src/main/java/util/MyTextSlider.java
@@ -1,15 +1,29 @@
 package util;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import gr.uoa.di.airbnbproject.R;
 
 /**
  * Created by sissy on 2/9/2017.
  */
 
-public class MyTextSlider extends TextSliderView {
+public class MyTextSlider extends MyBaseSliderView {
     public MyTextSlider(Context context) {
         super(context);
+    }
+
+    @Override
+    public View getView() {
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.render_type_text,null);
+        ImageView target = (ImageView)v.findViewById(R.id.daimajia_slider_image);
+        TextView description = (TextView)v.findViewById(R.id.description);
+        description.setText(getDescription());
+        bindEventAndShow(v, target);
+        return v;
     }
 }
